bf46e9bfc7a15cc941ca027e7af4692538672f59
Dave Moten <davidmoten@gmail.com>
2018-12-05 16:53:23 +1100

    update generate-site.sh

--- generate-site.sh
+++ generate-site.sh
@@ -3,8 +3,8 @@ set -e
 mvn site
 cd ../davidmoten.github.io
 git pull
-mkdir -p rxjava-file
-cp -r ../rxjava-file/target/site/* rxjava-file/
+mkdir -p rxjava-file2
+cp -r ../rxjava-file2/target/site/* rxjava-file2/
 git add .
 git commit -am "update site reports"
 git push
